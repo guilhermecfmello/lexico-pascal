299ee13161b3ad3fe048b8a37f98441323b44e7e
Guilherme Mello <guilhermecfmello@gmail.com>
2019-03-23 16:38:55 -0300

    lexico funcionando com alguns bugs, nao foi incluido o estado para o token \'diferente\'

--- lexico.py
+++ lexico.py
@@ -29,6 +29,8 @@ __     ___       _      _              ____           _       _
 '''
 
 
+from symbols import SymbolsTab
+
 #Dado um caractere pertencente ao alfabeto, retorna a coluna
 #correspondente na matriz de adjacencia
 def get_column(c):
@@ -79,6 +81,8 @@ def is_final(state):
         return 0
     elif state == 14:
         return 0
+    elif state > 24 or state < 0:
+        return 0
     else:
         return 1
 
@@ -116,14 +120,61 @@ def get_token(state):
         return "Numeral negativo"
     elif state == 21 or state == 22:
         return "Numeral positivo"
+    elif state == 23:
+        return "Mais"
+    elif state == 24:
+        return "Menos"
     else:
         return ""
 
+#Quando o automato termina no estado 18, deve-se saber qual eh o simbolo
+#lido e entao imprimir seu Token
+def get_special_token(c):
+    if c == ';':
+        return "Ponto e virgula"
+    elif c == ',':
+        return "Virgula"
+    elif c == '=':
+        return "Igual"
+    elif c == '*':
+        return "Asterisco"
+    elif c == '[':
+        return "Colchete_esq"
+    elif c == ']':
+        return "Colchete_dir"
+    elif c == '{':
+        return "Chave_esq"
+    elif c == '}':
+        return "Chave_dir"
 
+#Se o caractere passado estiver em [a,b], retorna 1
+#Retorna 0, caso contrario
+def is_char(c):
+    if c >= 'a' and c <= 'z':
+        return 1
+    else:
+        return 0
+
+#Trata identificadores
+def identifiers(dic, string, str_final):
+    cond_dic = dic.instalar_id(id)
+    #Se for uma palavra-chave
+    if cond_dic > 1:
+        str_final = str_final + id.upper()
+        # print("7:Token lido: " + id.upper())
+    elif cond_dic == 1:
+        # print("8:Token lido: Identificador("+ id.upper() + ") encontrado")
+        str_final = str_final + "Identificador(" + id.upper() + ") encontrado"
+    elif cond_dic == 0:
+        # print("9:Token lido: Identificador("+ id.upper() + ") armazenado")
+        str_final = str_final + "Identificador(" + id.upper() + ") armazenado"
+
+    str_final = str_final + "\n"
+    return str_final
 #   a-z, 0-9, . , ' , , , : , ) , = , * , [ , ] , { , } , < , > , ( ,  + , -
 
 states = [
-    [1 , 2  , 4 , 18, 18, 6, 17, 18, 18, 18, 18, 18, 18, 10, 8 , 12 , 21, 19], #Estado 0
+    [1 , 2  , 4 , 18, 18, 6, 17, 18, 18, 18, 18, 18, 18, 10, 8 , 12 , 23, 24], #Estado 0
     [1 , 1  , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 1
     [-1, 2  , 3 , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 2
     [-1, 3  , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 3
@@ -146,19 +197,35 @@ states = [
     [-1, 20 , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 20
     [-1, 21 , 22, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 21
     [-1, 22 , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 22
+    [-1, 21 , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1], #Estado 23
+    [-1, 19 , -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]  #Estado 24
 ]
 print("Analisador Lexico\nPara sair, digite \'exit()\'\n\n\n")
 line = input("Digite sua entrada de dados: ")
+
+#Inicializando tabela de palavras-chave/identificadores
+dic = SymbolsTab()
 while line != '' :
     if line == "exit()":
         break
+    str_final = ""
+    cond_error = 0
     next = 0
     current = 0
     last_final = 0
-    i = 0
     cm = 0
-    for j in range(len(line)):
+    j = 0
+    start_char = 0
+    end_char = 0
+    while(j < len(line)):
+    #for j in range(len(line)):
         c = line[j]
+        #Verifica se comecou a reconhecer uma palavra-chave/identificador
+        if is_char(c) and current == 0:
+            start_char = j
+
+
+        j = j + 1
         column = get_column(c)
         #Se o simbolo lido pertencer ao alfabeto
         if column > -1:
@@ -169,44 +236,132 @@ while line != '' :
                 #Se o proximo estado eh um estado final, confirmamos a leitura lida ate aqui
                 if is_final(next):
                     last_final = current
-                    curent = i
+                    cm = j
                     #Se chegou no final da linha
-                    if j == len(line) - 1:
-                        print("Token lido: " + get_token(last_final))
-                        i = cm
-                        current = 0
-                        last_final = 0
-                        next = 0
+                    if j == len(line):
+                        # print("TESTE STR_FINAL: " + str_final)
+
+                        #Se nao for um dos caracteres especiais, podera imprimir o token
+                        #do ultimo estado final, caso contrario, deve-se verificar qual
+                        #eh o token lido
+                        if last_final == 1:
+                            #Se terminar no estado 1 do automato, devera haver uma verificacao
+                            #na tabela de simbolos
+                            id = line[start_char:j]
+                            str_final = identifiers(dic, id, str_final)
+                            current = 0
+                            last_final = 0
+                            next = 0
+                            cm = j
+                        else:
+                            if last_final != 18:
+                                # print("1:Token lido: " + get_token(last_final)
+                                str_final = str_final + get_token(last_final) + "\n"
+                                # print(str_final)
+                                j = cm
+                                current = 0
+                                last_final = 0
+                                next = 0
+                            else:
+                                # print("4:Token lido: " + get_special_token(c))
+                                str_final = str_final + get_special_token(c) + "\n"
+
             #Se o caractere lido nao leva a um proximo estado
             else:
                 #Se o caractere lido nao leva a um proximo estado e o automato esta no estado inicial
                 #entao o char nao existe no alfabeto da linguagem, ou seja, erro lexico
                 if current == 0:
-                    print("ERRO")
-                    cm = i
+                    # print("ERRO")
+                    cm = j
                     current = 0
                     last_final = 0
                     next = 0
+                    if not cond_error:
+                        indexError = j
+                    cond_error = 1
                 #Se o caractere nao leva a um proximo estado, mas tb o automato nao esta no estado
                 #inicial, ainda pode ser aceito pela linguagem
                 else:
-                    i = cm
-                    print("Token lido: " + get_token(last_final))
-                    current = 0
-                    next = 0
-                    last_final = 0
+                    # j = cm
+                    #Se nao for um dos caracteres especiais, podera imprimir o token
+                    #do ultimo estado final, caso contrario, deve-se verificar qual
+                    #eh o token lido
+
+                    if last_final == 1:
+                        j = j - 1
+                        id = line[start_char:j]
+                        str_final = identifiers(dic, id, str_final)
+                        current = 0
+                        last_final = 0
+                        next = 0
+                        cm = j
+                    else:
+                        if last_final != 18:
+                            # print("2:Token lido: " + get_token(last_final))
+                            str_final = str_final + get_token(last_final) + "\n"
+                            j = cm
+                            current = 0
+                            next = 0
+                            last_final = 0
+                        else:
+                            # print("C: " + c + "line[j]: " + line[j])
+                            # print("5:Token lido: " + get_special_token(line[j-2]) + " | J: " + str(j))
+                            str_final = str_final + get_special_token(line[j-2]) + "\n"
+                            j = cm
+                            current = 0
+                            next = 0
+                            last_final = 0
+
+
         #Se o simbolo lido nao pertence ao alfabeto
         else:
             #Se o simbolo lido for espaco, nao ha erro lexico, apenas imprimimos o token lido
             #ate aqui, e resetamos o automato
-            if c == ' ':
-                print("Token lido: " + get_token(last_final))
-                i = cm
+            if c == ' ' and current != 0:
+                #Se nao for um dos caracteres especiais, podera imprimir o token
+                #do ultimo estado final, caso contrario, deve-se verificar qual
+                #eh o token lido
+                if last_final == 1:
+                    id = line[start_char:j - 1]
+                    str_final = identifiers(dic, id, str_final)
+                    current = 0
+                    last_final = 0
+                    next = 0
+                    cm = j
+                else:
+                    if last_final != 18:
+                        # print("3:Token lido: " + get_token(last_final))
+                        str_final = str_final + get_token(last_final) + "\n"
+                        j = cm
+                        current = 0
+                        last_final = 0
+                        next = 0
+                    else:
+                        # print("6:Token lido: " + get_special_token(line[j-2]))
+                        str_final = str_final + get_special_token(line[j-2]) + "\n"
+                        current = 0
+                        last_final = 0
+                        next = 0
+                        cm = j
+            #Se o simbolo lido eh um espaco, mas esta no estado inicial, devemos desconsidera-lo
+            elif c == ' ' and current == 0:
                 current = 0
                 last_final = 0
                 next = 0
+                cm = j
             else:
-                print("Erro lexico")
+                # print("Erro lexico")
+                if not cond_error:
+                    indexError = j
+                cond_error = 1
 
 
+    if cond_error:
+        print("\033[01;32m")
+        print("\nErro lexico em:\n\033[00;37m" + line)
+        for k in range(1, indexError):
+            print(" ", end = "")
+        print("\033[01;32m^\033[00;37m\n\n\n")
+    else:
+        print(str_final)
     line = input("Digite sua entrada de dados: ")
---
+++ symbols.py
@@ -0,0 +1,38 @@
+class SymbolsTab:
+    def __init__(self):
+        self.dic = {"and":25,"array":26,"asm":27,"begin":28,
+        "case":29, "const":30, "constructor": 31, "destructor":32,
+        "div":33, "do":34, "downto": 35, "else": 36, "End": 37,
+        "File": 38, "For": 39, "Foward": 40, "Function": 41, "Goto": 42,
+        "If": 43, "implementation": 44, "In": 45, "inline": 46,
+        "interface": 47, "Label": 48, "mod": 49, "nil": 50, "not": 51,
+        "object": 52, "of": 53, "or": 54, "packed": 55, "procedure": 56,
+        "program": 57, "record": 58, "repeat": 59, "set": 60, "shl": 61,
+        "shr": 62, "string": 63, "then": 64, "to": 65, "type": 66,
+        "unit": 67, "until": 68, "uses": 69, "var": 70, "while": 71,
+        "with": 72, "xor": 73}
+
+
+    #Acessa a tabela de simbolos, se a string existir na mesma e
+    #for uma palavra-chave, retorna seu token , caso nao seja
+    #palavra-chave retorna 1
+    #Se a string nao existir na tabela, adiciona e retorna 0
+    def instalar_id(self, string):
+        if string in self.dic:
+            # print("Existe no dic")
+            return self.dic[string]
+        else:
+            # print("Nao existe no dic")
+            self.dic[string] = 1
+            return 0
+
+
+    #Procura na tabela a ocorrencia de string
+    #Caso exista e seja uma palavra-chave, retorna seu token,
+    #caso nao seja uma palavra-chave e exista, retorna 1
+    #Caso nao exista na tabela, retorna 0
+    # def obter_token(self, string):
+    #     if(string in self.dic):
+    #         return self.dic["string"]
+    #     else:
+    #         return -1
